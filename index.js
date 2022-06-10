c3e14878a2bb62c0427c559ce26032e0e6cb8364
Ngaicode <mjombakevin376@gmail.com>
2022-06-10 22:59:36 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+"use strict";
+
+const testVar = "hi";
+const name = "Kevin";
+
+document.querySelector("main#main").remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = `${name} is the champion!`;
+newHeader.className = "victory";
